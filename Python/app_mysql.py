9c7922955ea267fd8899db36fc5bce6ec0c17c52
chandru1007 <60879663+chandru1007@users.noreply.github.com>
2020-09-06 22:21:31 +0530

    Update app_mysql.py

--- Python/app_mysql.py
+++ Python/app_mysql.py
@@ -19,37 +19,6 @@ mysql=MySQL(app)
 def home():
     return render_template('home_new.html')
 
-@app.route("/slam_home")
-def slam_home():
-    return render_template("slam_home.html")
-
-@app.route("/name")
-def name():
-    return render_template("name.html")
-
-@app.route("/buddy")
-def buddy():
-    return render_template('buddy.html')
-
-@app.route("/display",methods = ["POST","GET"])
-def display():
-    if request.method == "POST":
-        dat={}
-        dat['input1']      =   request.form['input1']
-        dat['input2']      =   request.form['input2']
-        dat['input3']      =   request.form['input3']
-        dat['input4']      =   request.form['input4']
-        dat['input5']      =   request.form['input5']
-        dat['input6']      =   request.form['input6']
-        dat['input7']      =   request.form['input7']
-        dat['input8']      =   request.form['input8']
-        dat['input9']      =   request.form['input9']
-        dat['input10']     =   request.form['input10']
-        dat['input11']     =   request.form['input11']
-        dat['input12']     =   request.form['input12']
-        dat['input13']     =   request.form['input13']
-        return render_template('display.html',rec = dat)
-
 @app.route("/admin")
 def admin():
     return render_template('admin.html')
